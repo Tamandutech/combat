1e0fa6a0ee8c58de011cca068fbcb55e2cf8acc4
Rafael Riciardi Barreira <rafael.r.barreira@hotmail.com>
2023-04-15 21:45:53 -0300

    Codigo ultima competicao

--- mushu/mushu.ino
+++ mushu/mushu.ino
@@ -7,9 +7,9 @@
 #define ledAmarelo 12
 #define weaponMotorPin 32
 
-#define SPEED_MIN (1000)  // Set the Minimum Speed in microseconds
-#define SPEED_MAX (2000)  
-#define SPEED_MED (1500)
+int SPEED_MIN = 1300;  // Set the Minimum Speed in microseconds
+int SPEED_MAX = 1700;
+int SPEED_MED = 1500;
 
 ESC motorEsquerdo (leftMotorPin, SPEED_MIN, SPEED_MAX, 500); 
 ESC motorDireito (rightMotorPin, SPEED_MIN, SPEED_MAX, 500);  
@@ -20,6 +20,7 @@ int lastState = 0;
 int velocidade = 0;
 int ve, vd, va;
 int locMaxSpeed = 300;
+int compensation = 0;
 
 void setup() {
   //PS4.begin("58:a0:23:33:d9:b0");
@@ -35,14 +36,14 @@ void setup() {
 
 
   while(!PS4.isConnected()){
-    Serial.println("Esperando Conexão");
+  //  Serial.println("Esperando Conexão");
     delay(250);
   }
 
   PS4.setLed(0,100,0); 
   PS4.sendToController();
 
-  Serial.println ("Conexão Estabelecida");
+  //Serial.println ("Conexão Estabelecida");
   
   motorEsquerdo.arm();
   motorDireito.arm();
@@ -52,29 +53,96 @@ void setup() {
 
 void loop() {
   if(PS4.isConnected()){
-      digitalWrite(ledVermelho, HIGH);
-      digitalWrite(ledAmarelo, HIGH);
       int LStickY= PS4.LStickY();
       int LStickX= PS4.LStickX();
-      
-      //Serial.print("X: ");
-      //Serial.print(LStickX);
-      //Serial.print("       Y: ");
-      //Serial.println (LStickY);
+
+      //Ajusta compensacao para andar reto
+      if(PS4.L1()){
+        if(compensation > -30){
+          compensation -= 1;
+          delay(500);
+        }
+      }
   
-      if(abs(LStickY) > abs(LStickX)){
+      if(PS4.R1()){
+        if(compensation < 30){
+          compensation += 1;
+          delay(500);
+        }
+      }
+
+      //Ajusta velocidade maxima
+      if(PS4.R2()){
+        if(SPEED_MAX < 2000 && SPEED_MIN > 1000){
+          SPEED_MAX += 25;
+          SPEED_MIN -= 25;
+          delay(500);
+        }
+      }
+  
+      if(PS4.L2()){
+        if(SPEED_MAX > 1525 && SPEED_MIN < 1475){
+          SPEED_MAX -= 25;
+          SPEED_MIN += 25;
+          delay(500);
+        }
+      }
+
+      //Teste inicio
+
+      if (PS4.Up()){ //frente
+          vd = SPEED_MAX+(compensation * 10);
+          ve = SPEED_MIN;
+  
+          motorEsquerdo.speed(ve);
+          motorDireito.speed(vd);
+        }
+  
+        else if(PS4.Down()){ //re
+          //vd = map(LStickY-compensation, -30, -127, SPEED_MED, SPEED_MIN);
+          //ve = map(LStickY+compensation, -30, -127, SPEED_MED, SPEED_MAX);
+          vd = SPEED_MIN;
+          ve = SPEED_MAX+(compensation * 10);
+  
+          motorEsquerdo.speed(ve);
+          motorDireito.speed(vd);
+        }
+      
+  
+      else if(PS4.Right()){
+          vd = SPEED_MAX;
+          ve = SPEED_MAX;
+  
+          motorEsquerdo.speed(ve);
+          motorDireito.speed(vd);
+        }
+  
+      else if(PS4.Left()){
+        vd = SPEED_MIN;
+        ve = SPEED_MIN;
+
+        motorEsquerdo.speed(ve);
+        motorDireito.speed(vd);
+      }
+      //Fim Teste
+  
+      /*if(abs(LStickY) > abs(LStickX)){
         //anda frente e tras
-        if (LStickY>=30){
-          vd = map(LStickY, 30, 127, SPEED_MED, SPEED_MAX);
+        if (LStickY>=30){ //frente
+          //vd = map(LStickY+compensation, 30, 127, SPEED_MED, SPEED_MAX);
+          //ve = map(LStickY-compensation, 30, 127, SPEED_MED, SPEED_MIN);
+          vd = map(LStickY, 30, 127, SPEED_MED, SPEED_MAX+(compensation * 10));
           ve = map(LStickY, 30, 127, SPEED_MED, SPEED_MIN);
   
           motorEsquerdo.speed(ve);
           motorDireito.speed(vd);
         }
   
-        else if(LStickY<=-30){
-          vd = map(LStickY, -30, -127, SPEED_MED, SPEED_MIN);
-          ve = map(LStickY, -30, -127, SPEED_MED, SPEED_MAX);
+        else if(LStickY<=-30){ //re
+          //vd = map(LStickY-compensation, -30, -127, SPEED_MED, SPEED_MIN);
+          //ve = map(LStickY+compensation, -30, -127, SPEED_MED, SPEED_MAX);
+          vd = map(LStickY-compensation, -30, -127, SPEED_MED, SPEED_MIN);
+          ve = map(LStickY+compensation, -30, -127, SPEED_MED, SPEED_MAX+(compensation * 10));
   
           motorEsquerdo.speed(ve);
           motorDireito.speed(vd);
@@ -110,7 +178,7 @@ void loop() {
           motorDireito.stop();
         }
       
-      }
+      }*/
   
       else{
         motorEsquerdo.stop();
@@ -136,7 +204,6 @@ void loop() {
   }
     //Fail safe
   else{
-    digitalWrite(ledVermelho, LOW);
     motorEsquerdo.speed(1500);
     motorDireito.speed(1500);
     motorArma.speed(1500);
@@ -144,7 +211,6 @@ void loop() {
     motorEsquerdo.stop();
     motorDireito.stop();
     motorArma.stop();
-    digitalWrite(ledAmarelo, LOW);
     delay(500);
     //Serial.println("Restart");
     setup();
